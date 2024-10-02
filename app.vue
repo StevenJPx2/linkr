7027ba879d0d6ed8cc6bf3a1ab46999503d28bb2
Steven John <stevenjpx2@gmail.com>
2024-10-02 17:30:11 +0530

    feat: finish first draft

--- app.vue
+++ app.vue
@@ -3,26 +3,50 @@ const links = await usePagesFile("links");
 </script>
 
 <template>
-  <div>
+  <main
+    class="mx-auto max-w-prose grid gap-12 py-16 px-4 sm:py-24 sm:px-6 lg:px-8"
+  >
     <NuxtRouteAnnouncer />
-    <div v-for="section in links.sections" :key="section.heading">
+
+    <section
+      v-for="section in links.sections"
+      :key="section.heading"
+      class="grid"
+    >
       <h2 class="text-2xl font-bold">
         {{ section.heading }}
       </h2>
 
-      <ul class="divide-y divide-gray-200">
-        <li
-          v-for="link in section.links"
-          :key="link.title"
-          class="flex items-center justify-between py-3"
-        >
-          <div class="flex items-center">
-            <NuxtLink :to="link.url" class="text-sm font-medium text-gray-900">
-              {{ link.title }}
-            </NuxtLink>
-          </div>
+      <p v-if="section.description" class="mt-3 leading-relaxed">
+        {{ section.description }}
+      </p>
+
+      <ul class="grid grid-cols-1 gap-2 mt-5">
+        <li v-for="link in section.links" :key="link.title">
+          <NuxtLink
+            :to="link.url"
+            class="flex items-center gap-2 py-3 rounded-lg border border-slate-400 hover:bg-blue-100 px-3 text-sm font-medium text-gray-900"
+          >
+            <Icon
+              v-if="link.icon"
+              :name="link.icon"
+              class="h-6 w-6 text-blue-950"
+            />
+            {{ link.title }}
+          </NuxtLink>
         </li>
       </ul>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
+
+<style>
+body {
+  @apply bg-blue-50;
+}
+
+main {
+  font-family: Manrope;
+  @apply text-blue-950;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-pagescms", "@nuxt/icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-pagescms",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+  ],
 });
